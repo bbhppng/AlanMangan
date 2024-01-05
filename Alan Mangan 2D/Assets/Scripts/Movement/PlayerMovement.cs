001cdac44b9da084c3b8a668d9ba9ba64d055f38
bbhppng <zykstas1@gmail.com>
2024-01-05 19:28:03 +0100

    advanced movement

---
+++ Alan Mangan 2D/Assets/Scripts/Movement/PlayerMovement.cs
@@ -0,0 +1,90 @@
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Movement
+{
+    public class Player : MonoBehaviour
+    {
+        [Header("Horizontal")]
+        [SerializeField] private float _speed = 5f;
+        [SerializeField] private bool _facingRight = true;
+        [SerializeField] private bool _airMove;
+        [FormerlySerializedAs("airMoveModificator")]
+        [Range(0, 1)] 
+        [SerializeField] private float _airMoveModificator;
+        
+        [FormerlySerializedAs("jumpForce")]
+        [Header("Vertical")]
+        [SerializeField] private float _jumpForce = 15f;
+        [SerializeField] private Transform _groundChecker;
+        [SerializeField] private float _groundCheckerRadius;
+        [SerializeField] private LayerMask _groundLayer;
+        
+        [FormerlySerializedAs("boxCollider2D")]
+        [Header("Crouch")] 
+        [SerializeField] private BoxCollider2D _boxCollider2D;
+        [SerializeField] private Transform _cellChecker;
+        [Range(0, 1)] 
+        [SerializeField] private float _crouchModificator;
+        
+        [SerializeField] private Rigidbody2D _rigidbody2D;
+        private float _direction;
+        private bool _jump;
+        void Update()
+        {
+            _direction = Input.GetAxisRaw("Horizontal");
+            if(Input.GetButtonDown("Jump"))
+            {
+                _jump = true;
+            }
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.DrawWireSphere(_groundChecker.position, _groundCheckerRadius);
+        }
+
+        private void FixedUpdate()
+        {
+            bool isGrounded = Physics2D.OverlapCircle(_groundChecker.position, _groundCheckerRadius, _groundLayer);
+            bool canStand = Physics2D.OverlapBox(_cellChecker.position, _boxCollider2D.size, _groundLayer);
+            
+            if (_jump && isGrounded)
+                _rigidbody2D.AddForce(Vector2.up * _jumpForce);
+            _jump = false;
+            
+            if (Input.GetAxis("Crouch") > 0 || !canStand)
+                _boxCollider2D.enabled = false;
+            else
+                _boxCollider2D.enabled = true;
+            
+            if (isGrounded || _airMove)
+            {
+                float speedModificator = 1;
+                if (!canStand)
+                    speedModificator = _crouchModificator;
+                else if (!isGrounded)
+                    speedModificator = _airMoveModificator;
+                _rigidbody2D.velocity = new Vector2(_direction * _speed * speedModificator, _rigidbody2D.velocity.y);
+                SetDirection();
+            }
+        }
+        private void SetDirection()
+        {
+            if (_facingRight && _direction < 0)
+            {
+                Flip();
+            }
+            else if (!_facingRight && _direction > 0)
+            {
+                Flip();
+            }
+        }
+        private void Flip()
+        {
+            _facingRight = !_facingRight;
+            transform.Rotate(0, 180, 0);
+        }
+    }
+}
+
